17e115576f9ec9210744087cf7ad7ee828318faa
Federico Cerisola <federico@cerisola.net>
2017-03-29 22:17:47 -0300

    [percolation] Added printing lattice to stdout

--- percolation/main.c
+++ percolation/main.c
@@ -1,11 +1,14 @@
 /* headers */
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /* auxiliary functions declarations */
 char * allocate_lattice(int rows, int columns);
 
 void populate_lattice(double probability, char * lattice, int rows, int columns);
+
+void print_lattice(char * lattice, int rows, int columns);
 
 /* main body function */
 int main()
@@ -23,6 +26,9 @@ int main()
     /* populate lattice with given probability */
     p = 0.4;
     populate_lattice(p, lattice, L, L);
+
+    /* print the generated lattice for visualization */
+    print_lattice(lattice, L, L);
 
     /* free memory before leaving */
     free(lattice);
@@ -61,4 +67,23 @@ void populate_lattice(double probability, char * lattice, int rows, int columns)
     }
 
     return;
+}
+
+void print_lattice(char * lattice, int rows, int columns)
+{
+    int i, j;
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < columns; j++) {
+            if (lattice[i + j*columns] == 1) {
+                putchar('x');
+            } else {
+                putchar('o');
+            }
+            if (j < columns - 1) {
+                putchar(' ');
+            }
+        }
+        putchar('\n');
+    }
 }
