d1f21c2b6c05a942e0c549589bd55d8bd67cff8d
Federico Cerisola <federico@cerisola.net>
2017-04-04 21:32:32 -0300

    [percolation] Fix documentation typo

--- percolation/lattice.h
+++ percolation/lattice.h
@@ -5,5 +5,5 @@
 /* Allocate lattice in memory */
 char * allocate_lattice(int rows, int columns);
 
-/* Populate lattice with geiven probability */
+/* Populate lattice with given probability */
 void populate_lattice(double probability, char * lattice, int rows, int columns);
