fd440beaf95128b12f2abc279525192bfcc5eca0
Federico Cerisola <federico@cerisola.net>
2017-04-04 22:54:54 -0300

    [percolation] Add passing problem parameters as program arguments

--- percolation/main.c
+++ percolation/main.c
@@ -7,21 +7,28 @@
 #include "io_helpers.h"
 
 /* main body function */
-int main()
+int main(int argc, char ** argv)
 {
     int L; /* square lattice size */
     double p; /* occupation probability of each lattice site */
     int *lattice; /* lattice array */
 
+    /* read input arguments; if none provided fallback to default values */
+    if (argc == 3) {
+        L = atoi(argv[1]);
+        p = atof(argv[2]);
+    } else {
+        L = 10;
+        p = 0.4;
+    }
+
     /* initialize random number generator seed */
     srand(time(NULL));
 
     /* allocate lattice */
-    L = 10;
     lattice = allocate_lattice(L, L);
 
     /* populate lattice with given probability */
-    p = 0.4;
     populate_lattice(p, lattice, L, L);
 
     /* print the generated lattice for visualization */
