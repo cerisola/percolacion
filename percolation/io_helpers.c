de5de4d4e22b2088cb4478b9b9321d632613c9a0
Federico Cerisola <federico@cerisola.net>
2017-04-08 16:32:20 -0300

    [percolation] Add function to write cluster statistics to file

--- percolation/io_helpers.c
+++ percolation/io_helpers.c
@@ -106,3 +106,51 @@ void write_lattice_to_file(const char * path, const int * lattice, int rows,
     free(seed_string);
     free(file_full_path);
 }
+
+void write_cluster_statistics_to_file(const char * path,
+                                      const int * cluster_sizes,
+                                      const int * cluster_sizes_counts,
+                                      int cluster_sizes_total_count,
+                                      int rows, int columns, int seed)
+{
+    int i;
+    size_t path_length;
+    size_t file_full_path_length;
+    char * file_full_path;
+    char * rows_string;
+    char * columns_string;
+    char * seed_string;
+    FILE * file_handler;
+
+    rows_string = (char *)malloc(12*sizeof(char));
+    sprintf(rows_string, "%d", rows);
+
+    columns_string = (char *)malloc(12*sizeof(char));
+    sprintf(columns_string, "%d", columns);
+
+    seed_string = (char *)malloc(12*sizeof(char));
+    sprintf(seed_string, "%d", seed);
+
+    path_length = strlen(path);
+    file_full_path_length  = path_length + 70;
+    file_full_path = (char *)malloc(file_full_path_length*sizeof(char));
+    strcpy(file_full_path, path);
+    strcat(file_full_path, "/clusters_");
+    strcat(file_full_path, rows_string);
+    strcat(file_full_path, "x");
+    strcat(file_full_path, columns_string);
+    strcat(file_full_path, "_");
+    strcat(file_full_path, seed_string);
+    strcat(file_full_path, ".csv");
+
+    file_handler = fopen(file_full_path, "w");
+    for (i = 0; i < cluster_sizes_total_count; i++) {
+        fprintf(file_handler, "%d,%d\n", cluster_sizes[i], cluster_sizes_counts[i]);
+    }
+
+    fclose(file_handler);
+    free(rows_string);
+    free(columns_string);
+    free(seed_string);
+    free(file_full_path);
+}
--- percolation/io_helpers.h
+++ percolation/io_helpers.h
@@ -24,3 +24,23 @@ void print_lattice(const int * lattice, int rows, int columns, int with_borders)
 */
 void write_lattice_to_file(const char * path, const int * lattice, int rows,
                            int columns, int seed);
+
+/*! Write cluster statistics to file.
+
+    @param path path to folder were the data will be writter. If the file exists
+        it will be overwritten.
+    @param cluster_sizes pointer to array with the different values of cluster
+        sizes.
+    @param cluster_sizes_counts pointer to array with the count of clusters that
+        have a given size (given by the respective element of the cluster_sizes
+        parameter).
+    @param cluster_sizes_total_count total amount of different cluster sizes.
+    @param rows the number of rows in the lattice.
+    @param columns the number of columns in the lattice.
+    @param seed the random number generator seed used to generate the lattice.
+*/
+void write_cluster_statistics_to_file(const char * path,
+                                      const int * cluster_sizes,
+                                      const int * cluster_sizes_counts,
+                                      int cluster_sizes_total_count,
+                                      int rows, int columns, int seed);
