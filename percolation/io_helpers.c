c50863236dfee37a2c6a8a6e801d7c54d5009d12
Federico Cerisola <federico@cerisola.net>
2017-04-08 16:13:35 -0300

    [percolation] Add function to write a lattice to a file

--- percolation/io_helpers.c
+++ percolation/io_helpers.c
@@ -1,5 +1,7 @@
 #include "io_helpers.h"
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 void print_lattice(const int * lattice, int rows, int columns, int with_borders)
 {
@@ -51,4 +53,56 @@ void print_lattice(const int * lattice, int rows, int columns, int with_borders)
     } else {
         putchar('\n');
     }
+}
+
+void write_lattice_to_file(const char * path, const int * lattice, int rows,
+                           int columns, int seed)
+{
+    int i;
+    int j;
+    size_t path_length;
+    size_t file_full_path_length;
+    char * file_full_path;
+    char * rows_string;
+    char * columns_string;
+    char * seed_string;
+    FILE * file_handler;
+
+    rows_string = (char *)malloc(12*sizeof(char));
+    sprintf(rows_string, "%d", rows);
+
+    columns_string = (char *)malloc(12*sizeof(char));
+    sprintf(columns_string, "%d", columns);
+
+    seed_string = (char *)malloc(12*sizeof(char));
+    sprintf(seed_string, "%d", seed);
+
+    path_length = strlen(path);
+    file_full_path_length  = path_length + 70;
+    file_full_path = (char *)malloc(file_full_path_length*sizeof(char));
+    strcpy(file_full_path, path);
+    strcat(file_full_path, "/lattice_");
+    strcat(file_full_path, rows_string);
+    strcat(file_full_path, "x");
+    strcat(file_full_path, columns_string);
+    strcat(file_full_path, "_");
+    strcat(file_full_path, seed_string);
+    strcat(file_full_path, ".csv");
+
+    file_handler = fopen(file_full_path, "w");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < columns; j++) {
+            fprintf(file_handler, "%d", lattice[i*columns + j]);
+            if (j < columns - 1) {
+                fputs(",", file_handler);
+            }
+        }
+        fputs("\n", file_handler);
+    }
+
+    fclose(file_handler);
+    free(rows_string);
+    free(columns_string);
+    free(seed_string);
+    free(file_full_path);
 }
--- percolation/io_helpers.h
+++ percolation/io_helpers.h
@@ -12,3 +12,15 @@
         borders around each cell.
 */
 void print_lattice(const int * lattice, int rows, int columns, int with_borders);
+
+/*! Write lattice to file.
+
+    @param path path to folder were the lattice will be written. If the file
+        exists it will be overwritten.
+    @param lattice pointer to lattice to be written.
+    @param rows the number of rows in the lattice.
+    @param columns the number of columns in the lattice.
+    @param seed the random generator seed used to generate the lattice.
+*/
+void write_lattice_to_file(const char * path, const int * lattice, int rows,
+                           int columns, int seed);
