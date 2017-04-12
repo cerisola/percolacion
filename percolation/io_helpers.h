eae1b9251681d283602349b75aa289500eabd097
Federico Cerisola <federico@cerisola.net>
2017-04-11 23:35:13 -0300

    [percolation] Fix a couple of documentation typos

--- percolation/io_helpers.h
+++ percolation/io_helpers.h
@@ -15,7 +15,7 @@ void print_lattice(const int * lattice, int rows, int columns, int with_borders)
 
 /*! Write lattice to file.
 
-    @param path path to folder were the lattice will be written. If the file
+    @param path path to the folder were the lattice will be written. If the file
         exists it will be overwritten.
     @param lattice pointer to lattice to be written.
     @param rows the number of rows in the lattice.
@@ -28,8 +28,8 @@ void write_lattice_to_file(const char * path, const int * lattice, int rows,
 
 /*! Write cluster statistics to file.
 
-    @param path path to folder were the data will be writter. If the file exists
-        it will be overwritten.
+    @param path path to the folder where the data will be written. If the file
+        exists it will be overwritten.
     @param cluster_sizes pointer to array with the different values of cluster
         sizes.
     @param cluster_sizes_counts pointer to array with the count of clusters that
