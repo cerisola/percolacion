86f26ad04d661a4e7342d66c48b0cd4053e3b08e
Federico Cerisola <federico@cerisola.net>
2017-04-16 00:54:30 -0300

    [percolation] Fix incorrect array initialization in cluster labelling function

--- percolation/clusters.c
+++ percolation/clusters.c
@@ -20,7 +20,7 @@ void label_clusters(int * lattice, int rows, int columns)
 
     /* allocate and initialize auxilliary label storing array */
     labels = (int *) malloc((lattice_size/2 + 2)*sizeof(int));
-    for (i = 0; i < lattice_size/2; i++) {
+    for (i = 0; i < lattice_size/2 + 2; i++) {
         labels[i] = i;
     }
 
