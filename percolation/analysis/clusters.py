a658b1268f8a2a31d3a5cb56ab0b12f8290d474c
Federico Cerisola <federico@cerisola.net>
2017-04-22 19:52:09 -0300

    [percolation] Add functions to calculate cluster statistics averages over many realizations

--- percolation/analysis/clusters.py
+++ percolation/analysis/clusters.py
@@ -37,3 +37,17 @@ def percolating_cluster_density_list(size, percolated, L):
 
 def percolating_cluster_strength_list(size, percolated, L):
     return percolating_cluster_mass_list(size, percolated)/(L*L)
+
+
+# % Averaged values over many realizations % #
+def percolating_cluster_mass_average(size, percolated, p_percolation):
+    return percolating_cluster_mass_list(size, percolated) * p_percolation
+
+
+def percolating_cluster_density_average(size, percolated, p_percolation, L):
+    return percolating_cluster_mass_average(size, percolated, p_percolation)/(L*L)
+
+
+def percolating_cluster_strength_average(size, percolated, p_percolation, L):
+    return percolating_cluster_mass_average(size, percolated, p_percolation)/(L*L)
+
